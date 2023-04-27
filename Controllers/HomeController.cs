f5a21dbc5e0225e746c0397146f1281e54e0c60b
Danyelle Gomes <100481070+DanyGRS@users.noreply.github.com>
2023-04-27 01:19:52 -0300

    Commit Initial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Office.Interop.Excel;
+
+namespace teste.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult ChamarMacro()
+        {
+            Application xlApp = new Application();
+            Workbook xlWorkbook = xlApp.Workbooks.Open(@"C:\Users\PC\source\repos\teste\teste\certi.xlsx");
+            xlApp.Run("GerarCertificados");
+            xlWorkbook.Close();
+            xlApp.Quit();
+            return View();
+        }
+    }
+}
